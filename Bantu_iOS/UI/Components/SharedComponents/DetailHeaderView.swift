804e7d8e00ea1b7cb13f624bcd178b4c848fc3cd
inmiti <inmabauvel@gmail.com>
2024-06-04 19:58:39 +0200

    Avanzo en detail

---
+++ Bantu_iOS/UI/Components/SharedComponents/DetailHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailHeaderView.swift
+//  Bantu_iOS
+//
+//  Created by ibautista on 4/6/24.
+//
+
+import SwiftUI
+
+struct DetailHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailHeaderView()
+    }
+}
--- Bantu_iOS/UI/DetailProfessionalView/DetailProfessionalView.swift
+++ Bantu_iOS/UI/DetailProfessionalView/DetailProfessionalView.swift
@@ -10,11 +10,58 @@ import SwiftUI
 struct DetailProfessionalView: View {
     var professional: User
     var body: some View {
-        VStack{
-            if let name = professional.name  {
-                Text(name)
+        ZStack {
+            
+            Rectangle()
+                .frame(height: 430)
+                .foregroundColor(.white)
+                .ignoresSafeArea(edges: .top)
+            
+            Rectangle()
+                .fill(LinearGradient(
+                    colors: [Color.bantu_orange, Color.bantu_pink],
+                    startPoint: .bottom,
+                    endPoint: .top)
+                )
+//                .opacity(0.90)
+                .ignoresSafeArea()
+                .frame(height: 400)
+            
+            VStack{
+               Spacer()
+               
+                AsyncImage(url: URL(string: professional.photo ?? "person.circle")) { image in
+                    image
+                        .resizable()
+                        .frame(width: 180, height: 180)
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(30)
+                        .shadow(color: .black.opacity(0.5), radius: 4, x: 0, y: 4)
+                }
+
+            placeholder: {
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .frame(width: 180, height: 180)
+                        .clipShape(Circle())
+                        .foregroundColor(.gray)
+
+                }
+                
+                if let name = professional.name, let firstname = professional.lastName1 {
+                    Text("\(name) \(firstname)")
+                        .padding(.top, 48)
+                        .padding(.bottom, 24)
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        
+                }
+                    
+               
             }
-           
+            .frame(height: 400)
+            
         }
     }
 }
@@ -33,7 +80,7 @@ struct DetailProfessionalView_Previews: PreviewProvider {
             postalCode: "28001",
             email: "dmunoz@bantu.com",
             city: "Madrid",
-            photo: "https://www.dropbox.com/scl/fi/tzvx69ys248s3tlzxygzj/user04.png?rlkey=fbpe3s43c3r5v6z7417gvgu70&st=d60ha2xz&dl=0",
+            photo: "https://cdn.alfabetajuega.com/alfabetajuega/2020/12/goku1.jpg?width=300",
             password: "$2b$12$xFyrpIOgKuu1awuFNPNn8OQRkKXS9uh7au0A.3S/BUz3SwURnjR3y",
             active: true,
             professional: Professional(description: "Cocinero español especializado en cocina de vanguardia. Su restaurante DiverXo ha recibido tres estrellas Michelin",
